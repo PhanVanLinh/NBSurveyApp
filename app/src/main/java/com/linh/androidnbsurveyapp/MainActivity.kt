72e0eaf0e34d015f225572e5bc01fb48555e71f2
Phan Van Linh <phanvanlinh.94vn@gmail.com>
2021-06-26 15:40:06 +0700

    [Feature] Configure client id and client secret

--- app/src/main/java/com/linh/androidnbsurveyapp/MainActivity.kt
+++ app/src/main/java/com/linh/androidnbsurveyapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.linh.androidnbsurveyapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        BuildConfig.OAUTH_CLIENT_ID
     }
 }
